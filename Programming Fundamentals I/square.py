3a50e805f4fdd1e28041324063fd689d2bb36c43
Jackson Zenisek <115522316+JacksonZenisek@users.noreply.github.com>
2026-04-04 13:54:57 -0500

    Delete Programming Fundamentals I/square.py

--- Programming Fundamentals I/square.py
+++
@@ -1,7 +0,0 @@
-# Defines the calculation of the area of a square:
-def square_area(side):
-    return side ** 2
-
-# Defines the calculation of the perimeter of a square:
-def square_perimeter(side):
-    return 4 * side
